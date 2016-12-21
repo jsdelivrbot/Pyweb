cb6597e56ecb8adab8041937660ce54b2f76d66a
Graham Misail <gmisail@Grahams-Macintosh.local>
2016-12-20 20:58:13 -0500

    versatile lib

--- game.py
+++ game.py
@@ -3,23 +3,27 @@ from browser import window
 app = window.getLibrary()
 app.start()
 
-sprite = app.sprite("ship.png")
-app.add(sprite)
+speed = 3
+
+left = app.sprite("paddle.png")
+left.setX(8)
+app.add(left)
+
+right = app.sprite("paddle.png")
+right.setX(800 - 24)
+app.add(right)
 
 def update():
     if(app.input.check("W")):
-        sprite.setY(sprite.getY() - 1)
+        left.setY(left.getY() - speed)
 
     if(app.input.check("S")):
-        sprite.setY(sprite.getY() + 1)
+        left.setY(left.getY() + speed)
 
-    if(app.input.check("A")):
-        sprite.setX(sprite.getX() - 1)
+    if(app.input.check("I")):
+        right.setY(right.getY() - speed)
 
-    if(app.input.check("D")):
-        sprite.setX(sprite.getX() + 1)
-
-    if(app.input.mouse.down):
-        app.log("Downnnnn")
+    if(app.input.check("K")):
+        right.setY(right.getY() + speed)
 
 app.loop(update)
